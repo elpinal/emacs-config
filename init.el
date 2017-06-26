4694eb61b29fc5f6448ff0c321e9c59cd2a0fa49
elpinal <elpinal@users.noreply.github.com>
2017-06-26 20:52:43 +0900

    Stop the access to the repository of packages
    This is an ad hoc fix.

--- init.el
+++ init.el
@@ -49,7 +49,7 @@
 (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize)
-(package-refresh-contents)
+;(package-refresh-contents)
 
 (defconst packages-to-install
   '(
